a59782eafb47a913857831371c5cb4c80f4c01cb
Raphael F <raphael.fontes@ifs.edu.br>
2020-12-18 08:01:03 -0300

    Added model history middleware

--- vaccine_card/core/settings/base.py
+++ vaccine_card/core/settings/base.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'simple_history.middleware.HistoryRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'vaccine_card.urls'
