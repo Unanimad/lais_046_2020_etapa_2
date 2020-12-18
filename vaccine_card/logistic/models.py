e29a7a4365a5a93430132222c12bd69b09e41bb2
Raphael F <raphael.fontes@ifs.edu.br>
2020-12-18 12:31:34 -0300

    Logistic
    App responsible to management vaccines stock and health centers
    
    Create model to:
    - Stock
    - VaccineStock
    - Address
    - City
    - State
    - HealthCenter
    
    Add field city to address
    Add field state to address
    Add field health_center to stock

--- vaccine_card/logistic/models.py
+++ vaccine_card/logistic/models.py
@@ -1,7 +1,55 @@
 from django.db import models
 
-
 from vaccine_card.vaccination.models import Vaccine
+
+
+class State(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Nome')
+
+    class Meta:
+        verbose_name = 'Unidade Federativa'
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Nome')
+
+    class Meta:
+        verbose_name = 'Município'
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    logradouro = models.CharField(max_length=150, verbose_name='Logradouro')
+    numero = models.CharField(max_length=4, verbose_name='Número')
+    complemento = models.CharField(max_length=50, null=True, blank=True, verbose_name='Complemento')
+    bairro = models.CharField(max_length=150, verbose_name='Bairro')
+    cep = models.CharField(max_length=8, verbose_name='CEP')
+
+    state = models.ForeignKey(State, on_delete=models.CASCADE, verbose_name=State._meta.verbose_name)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name=City._meta.verbose_name)
+
+    class Meta:
+        verbose_name = 'Endereço'
+
+
+class HealthCenter(models.Model):
+    cnes = models.CharField(max_length=7, verbose_name='CNES')
+    cnpj = models.CharField(max_length=14, verbose_name='CNPJ')
+    razao_social = models.CharField(max_length=255, verbose_name='Razão Social')
+
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Criado em:')
+    updated_at = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name='Atualizado em:')
+
+    address = models.ManyToManyField(Address, verbose_name=Address._meta.verbose_name)
+
+    class Meta:
+        verbose_name = 'Estabelecimento de Saúde'
+        verbose_name_plural = 'Estabelecimentos de Saúde'
 
 
 class Stock(models.Model):
@@ -9,6 +57,9 @@ class Stock(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name='Criado em:')
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name='Atualizado em:')
+
+    health_center = models.ForeignKey(HealthCenter, on_delete=models.CASCADE,
+                                      verbose_name=HealthCenter._meta.verbose_name)
 
     vaccines = models.ManyToManyField(Vaccine, through='VaccineStock', verbose_name=Vaccine._meta.verbose_name)
 
