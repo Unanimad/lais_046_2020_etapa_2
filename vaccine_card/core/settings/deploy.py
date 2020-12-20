1ffda4aa69c6cb80f268eaf88722df56061bf513
Raphael F <raphael.fontes@ifs.edu.br>
2020-12-20 16:35:08 -0300

    Debug mode on deployment

--- vaccine_card/core/settings/deploy.py
+++ vaccine_card/core/settings/deploy.py
@@ -4,6 +4,6 @@ from vaccine_card.core.settings.base import *
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
-DEBUG = False
+DEBUG = True
 DATABASE_URL = os.environ.get('DATABASE_URL', '')
 DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
