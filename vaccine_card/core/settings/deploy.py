069ddaca88a9ee8e53c61ec942b48f39203a297b
Raphael F <raphael.fontes@ifs.edu.br>
2020-12-20 14:55:12 -0300

    Procfile

--- vaccine_card/core/settings/deploy.py
+++ vaccine_card/core/settings/deploy.py
@@ -1,5 +1,3 @@
-import os
-
 import dj_database_url
 
 from vaccine_card.core.settings.base import *
@@ -7,5 +5,5 @@ from vaccine_card.core.settings.base import *
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 DEBUG = False
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
